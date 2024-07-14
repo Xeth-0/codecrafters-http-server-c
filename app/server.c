a051da78bfb60ed08ac3f9a3b4df97c3184b78af
Theodorus-VII <tediberhanu777@gmail.com>
2024-07-14 12:38:55 +0300

    return simple http 200 response

--- app/server.c
+++ app/server.c
@@ -7,55 +7,75 @@
 #include <errno.h>
 #include <unistd.h>
 
-int main() {
+int main()
+{
 	// Disable output buffering
 	setbuf(stdout, NULL);
- 	setbuf(stderr, NULL);
+	setbuf(stderr, NULL);
 
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	printf("Logs from your program will appear here!\n");
 
 	// Uncomment this block to pass the first stage
 	//
-	int server_fd, client_addr_len;
+	int server_fd, client_addr_len, new_socket;
 	struct sockaddr_in client_addr;
 	//
-	server_fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (server_fd == -1) {
+	server_fd = socket(AF_INET, SOCK_STREAM, 0); // AF_INET => IPv4 || SOCK_STREAM => TCP || 0 => IP protocol
+	if (server_fd == -1)
+	{
 		printf("Socket creation failed: %s...\n", strerror(errno));
 		return 1;
 	}
-	
+
 	// Since the tester restarts your program quite often, setting SO_REUSEADDR
 	// ensures that we don't run into 'Address already in use' errors
 	int reuse = 1;
-	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
+	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0)
+	{
 		printf("SO_REUSEADDR failed: %s \n", strerror(errno));
 		return 1;
 	}
-	
-	struct sockaddr_in serv_addr = { .sin_family = AF_INET ,
-									 .sin_port = htons(4221),
-									 .sin_addr = { htonl(INADDR_ANY) },
-									};
-	
-	if (bind(server_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) != 0) {
+
+	struct sockaddr_in serv_addr = {
+		.sin_family = AF_INET,			 // Address family
+		.sin_port = htons(4221),		 // Port number
+		.sin_addr = {htonl(INADDR_ANY)}, // Internet address(struct)
+	}; // No padding provided.
+
+	// (struct sockaddr *)&serv_addr => It's casting the pointer(address)
+	// of serv_addr: from type (sockaddr_in *) - to - (sockaddr *)
+	if (bind(server_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) != 0)
+	{
 		printf("Bind failed: %s \n", strerror(errno));
 		return 1;
 	}
-	
+
 	int connection_backlog = 5;
-	if (listen(server_fd, connection_backlog) != 0) {
+	if (listen(server_fd, connection_backlog) != 0)
+	{
 		printf("Listen failed: %s \n", strerror(errno));
 		return 1;
 	}
 	//
 	printf("Waiting for a client to connect...\n");
 	client_addr_len = sizeof(client_addr);
-	
-	accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
+
+	new_socket = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
+	if (new_socket == -1)
+	{
+		printf("Accept failed: %s \n", strerror(errno));
+		return 1;
+	}
+
 	printf("Client connected\n");
+	printf("Socket FN %d", new_socket);
+
+	// Send http 200 response
+	char* response = "HTTP/1.1 200 OK\r\n\r\n";
+	send(new_socket, response, strlen(response),0);
 	
+	close(new_socket);
 	close(server_fd);
 
 	return 0;
